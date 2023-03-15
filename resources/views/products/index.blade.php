9e869dcae420950c94aff962586c6c0f45234349
MitVithani <vithanimit@gmail.com>
2023-03-15 19:28:25 +0530

    responsiv product screen

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,9 +4,9 @@
     <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link href="{{ asset('public/css/image-uploader.min.css') }}" rel="stylesheet">
     <style>
-        body {
+        /* body {
             overflow-y: hidden;
-        }
+        } */
     </style>
 @endsection
 @section('navbar_header')
@@ -26,30 +26,42 @@
         <span class="float-right">
         <a class="btn btn-success" href="{{ route('products.create') }}" >+</a>
         </span>
-        @foreach ($products as $product)
-        <hr>
-        <input name="product_ids" type="hidden" value="{{$product->id}}"/>
-        <div class="mt-2">
-            <div class="col-md-12 row">
-                <div class="col-md-3">
-                    {{-- <img class="product-img" src="{{asset('public/img/empty_cart.png')}}" /> --}}
-                    <img class="product-img" src="{{ !empty($product->productImage[0]->image) ? asset($product->productImage[0]->image) : ''}}" />
 
-                </div>
-                <div class="col-md-3 align-self-center text-center">
-                    <span class="minus" onclick='ChangeQuantity("minus", {{$product->id}})'>-</span>
-                    <input id="qun_{{$product->id}}" name="quantity" type="text" disabled value="{{$product->quantity}}"/>
-                    <span class="plus" onclick='ChangeQuantity("plus", {{$product->id}})'>+</span>
-                </div>
-                <div class="col-md-3 align-self-center text-center">
-                    <b>USD <span id="product_price_{{$product->id}}">{{$product->price}}</span></b>
-                </div>
-                <div class="col-md-3 align-self-center text-center">
-                    <b>USD <span name="product_price_total" id="product_price_total_{{$product->id}}" >0</span></b>
-                </div>
-            </div>
-        </div>
-        @endforeach
+        <table class="table product-table">
+            <tbody>
+                @foreach ($products as $product)
+                    <tr>
+                        <td>
+                            <div class="">
+                                <input name="product_ids" type="hidden" value="{{$product->id}}"/>
+                                {{-- <img class="product-img" src="{{asset('public/img/empty_cart.png')}}" /> --}}
+                                <img class="product-img" src="{{ !empty($product->productImage[0]->image) ? asset($product->productImage[0]->image) : ''}}" />
+
+                            </div>
+                        </td>
+                        <td>
+                            <div class="align-self-center text-center">
+                                <span class="minus" onclick='ChangeQuantity("minus", {{$product->id}})'>-</span>
+                                <input id="qun_{{$product->id}}" name="quantity" type="text" disabled value="{{$product->quantity}}"/>
+                                <span class="plus" onclick='ChangeQuantity("plus", {{$product->id}})'>+</span>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="align-self-center text-center">
+                                <b>USD <span id="product_price_{{$product->id}}">{{$product->price}}</span></b>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="align-self-center text-center">
+                                <b>USD <span name="product_price_total" id="product_price_total_{{$product->id}}" >0</span></b>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+
         <hr>
 
     </div>
@@ -79,33 +91,34 @@
                 <b>USD <span id="sendorder">0</span></b>
             </div>
         </div>
-
-        <table>
-            <tr>
-                <td>
-                    <a href="{{ url('usersProducts'). '/' . $user_id}}">
-                        <img class="share-img" src="{{asset('public/img/whatsapp.png')}}">
-                    </a>
-                </td>
-                <td>
-                    <img class="share-img" src="{{asset('public/img/qr-code-scan.png')}}">
-                </td>
-                <td>
-                    <img class="share-img" src="{{asset('public/img/link.png')}}">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    WhatsApp
-                </td>
-                <td>
-                    QR Code
-                </td>
-                <td>
-                    Copy Link
-                </td>
-            </tr>
-        </table>
+        <div class="share-icon-div">
+            <table class="">
+                <tr>
+                    <td>
+                        <a href="{{ url('usersProducts'). '/' . $user_id}}">
+                            <img class="share-img" src="{{asset('public/img/whatsapp.png')}}">
+                        </a>
+                    </td>
+                    <td>
+                        <img class="share-img" src="{{asset('public/img/qr-code-scan.png')}}">
+                    </td>
+                    <td>
+                        <img class="share-img" src="{{asset('public/img/link.png')}}">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        WhatsApp
+                    </td>
+                    <td>
+                        QR Code
+                    </td>
+                    <td>
+                        Copy Link
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
 
 
