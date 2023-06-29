67390106ffd7c4b500b53975e07c0676304daa49
MitVithani <vithanimit@gmail.com>
2023-06-29 21:08:56 +0530

    Change route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'SellerController@index')->name('home');
 
 
 Auth::routes();
